38c1891d80b22a93b7384d7425836a2186899030
Luvaha <luvaha456@gmail.com>
2025-07-20 00:24:13 +0300

    Define API Routes: Authentication Routes

---
+++ app/routes/auth_routes.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from .. import models, schemas, auth, database
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+# -------------- SignUp-------------------
+@router.post("/signup", response_model=schemas.UserOut)
+def signup(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    if db.query(models.User).filter(models.User.email == user.email).first():
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_pw = auth.get_password_hash(user.password)
+    db_user = models.User(username=user.username, email=user.email, password=hashed_pw)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# ---------------- Login -------------------
+@router.post("/login", response_model=schemas.Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = auth.create_access_token(data={"sub": user.username})
+    return {"access_token": token, "token_type": "bearer"}
